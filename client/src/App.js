8f95004fed3bb0f8544f77d1beb898137c595a6e
Peter Lavigne <peterklavigne@gmail.com>
2019-11-10 15:37:04 -0500

    remove all previous spotitalk client code

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SongSearch from './components/SongSearch';
-import SongSoloView from './components/SongSoloView';
-import SongSearchDispatcher from './dispatchers/songSearchDispatcher';
-import { authEndpoint, clientId, redirectUri, scopes, showDialogue } from './config';
-import hash from "./hash";
 
 class App extends React.Component {
-  componentDidMount() {
-    const { saveAuthToken } = this.props;
-    let _token = hash().access_token;
-    if (_token) {
-      saveAuthToken(_token);
-    }
-  }
-
-  getAuthorizationEndpoint() {
-    return `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=${showDialogue}`;
-  }
-
   render() {
+    //<Route exact path="/" component={/* todo */} />
     return (
-      <div className="App">
-        <header className="App-header">
-          {!this.props.authToken && (
-            <a
-              className="btn btn--loginApp-link"
-              href={this.getAuthorizationEndpoint()}
-            >
-              Login to SpotiTalk
-            </a>
-          )}
-          {this.props.authToken && (
-            <div>
-              <Router>
-                <div>
-                  <Route exact path="/" render={() => <SongSearch {...this.props} />} />
-                  <Route exact path="/SongSoloView" component={SongSoloView} />
-                </div>
-              </Router>
-            </div>
-          )}
-        </header>
+      <div className='App'>
+        <Router>
+          <div>
+            // <Route exact path="/songs/:id" component={SongView} />
+          </div>
+        </Router>
       </div>
     );
   }
 }
-export default connect(state => state.songs, SongSearchDispatcher)(App);
+
+export default App;
+
--- client/src/config.js
+++
@@ -1,7 +0,0 @@
-const authEndpoint = 'https://accounts.spotify.com/authorize';
-const clientId = "e0dcd5b3317a4cb79aa12cc8d2172250";
-const redirectUri = "http://localhost:3000/";
-const scopes = [];
-const showDialogue = false;
-
-export { authEndpoint, clientId, redirectUri, scopes, showDialogue };
