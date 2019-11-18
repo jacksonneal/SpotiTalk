4b9280b24c0a6c168dea426a6cda3a9c25e6128b
JacksonNeal <neal.ja@husky.neu.edu>
2019-11-18 00:03:58 -0500

    add collapse for post form

--- client/src/components/forum/Forum.js
+++ client/src/components/forum/Forum.js
@@ -1,6 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Collapse } from 'react-bootstrap';
 
 export default function Forum(props) {
-    console.log(props);
-    return <div>Forum</div>
+    const [open, setOpen] = useState(false);
+
+    const results = [{
+        id: 1,
+        title: "First Post!",
+        content: "This is an example post",
+        subject: "Wow, isn't this great",
+        time: '12:00 pm',
+        src: 'https://images.unsplash.com/photo-1496196614460-48988a57fccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
+        date: '2019-10-10',
+        comments: [
+            {
+                id: 1,
+                content: "Wow greate post."
+            }
+        ]
+    }, {
+        id: 1,
+        title: "First Post!",
+        content: "This is an example post",
+        subject: "Wow, isn't this great",
+        time: '12:00 pm',
+        src: 'https://images.unsplash.com/photo-1496196614460-48988a57fccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
+        date: '2019-10-10',
+        comments: [
+            {
+                id: 1,
+                content: "Wow greate post."
+            }
+        ]
+    }, {
+        id: 1,
+        title: "First Post!",
+        content: "This is an example post",
+        subject: "Wow, isn't this great",
+        time: '12:00 pm',
+        src: 'https://images.unsplash.com/photo-1496196614460-48988a57fccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
+        date: '2019-10-10',
+        comments: [
+            {
+                id: 1,
+                content: "Wow greate post."
+            }
+        ]
+    }];
+    return (
+        <div>
+            {results.map(result =>
+                <div id={result.id} className="row post">
+                    <div className="col-12">
+                        <div className="panel-heading post-header">
+                            <span className="pull-left">
+                                #{result.id}
+                            </span>
+                            <span className="pull-left">
+                                {result.title}
+                            </span>
+                            <time class="pull-right">
+                                <span className="calendar">
+                                    <i class="fa fa-calendar"></i>
+                                    {result.time}
+                                </span>
+                                <span className="clock">
+                                    <i class="fa fa-clock-o"></i>
+                                </span>
+                                {result.time}
+                            </time>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <div className="post-body">
+                            <div className="row">
+                                <div className="col-8">
+                                    <div class="row">
+                                        <h3 class="subject-line">
+                                            {result.subject}
+                                        </h3>
+                                    </div>
+                                    <div>
+                                        <hr className="break-line" />
+                                    </div>
+                                    <div class="row">
+                                        <p class="post-content">
+                                            {result.content}
+                                        </p>
+                                    </div>
+                                </div>
+                                <hr className="break-line" />
+                                <div className="col-4">
+                                    <img className="image-box" src={result.src} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <div className="post-footer">
+                            <span class="pull-left">
+                                Like
+                            </span>
+                            <span class="pull-right">
+                                Reply
+                            </span>
+                        </div>
+                    </div>
+                </div>)}
+            <Collapse className="post-form fixed-bottom" in={open}>
+                <div>
+                    <input class="form-control form-control-lg" type="text" placeholder="Title" />
+                    <input class="form-control" type="text" placeholder="Subject" />
+                    <textarea rows={3} class="form-control form-control-sm" type="text" placeholder="Content" />
+                    <button>Post</button>
+                </div>
+            </Collapse>
+            <footer className="navbar fixed-bottom create-post">
+                <Button
+                    onClick={() => setOpen(!open)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                >
+                    Create Post
+                </Button>
+            </footer>
+        </div>)
 }
