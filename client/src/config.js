6ab529a37c51a8280e98ae2820fd21535289a62d
JacksonNeal <neal.ja@husky.neu.edu>
2019-12-01 17:51:53 -0500

    config

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
 //const api = 'https://spotitalk-database.herokuapp.com/api';
 //const api = 'http://localhost:5000/api';
-const api = 'api';
+//const api = 'api';
+const api = 'https://spotitalk.herokuapp.com/api'
 export { api };
